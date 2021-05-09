6815da14919deac996046fef0587496c6b0f3deb
ayushhagarwal <ayushgrwl365@gmail.com>
2021-05-09 11:54:31 +0530

    project rendering via flask complete

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for,request,redirect
+from flask import Response
+import mysql.connector
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__=="__main__":
+    app.run(host ='0.0.0.0', port = 5001, debug = True)     
