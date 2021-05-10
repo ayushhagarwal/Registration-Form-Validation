dd4a56aedb07784efd6f5bf81a2089fa9603c420
ayushhagarwal <ayushgrwl365@gmail.com>
2021-05-10 17:25:54 +0530

    flask setup complete for all the pages

--- app.py
+++ app.py
@@ -6,13 +6,23 @@ app=Flask(__name__)
 
 
 
-
 cursor=conn.cursor()
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/success")
+def success():
+    return render_template("submit.html")
+
+@app.route("/failed")
+def failed():
+    return render_template("duplicate.html")
+
+@app.route('/submit',methods=['POST'])
+def submit():
+    return redirect('/success')
 
 if __name__=="__main__":
     app.run(host ='0.0.0.0', port = 5001, debug = True)     
